845c1496cdef1210baae4a144f0e83ad9ebb0b72
val461 <6820182+val461@users.noreply.github.com>
2025-09-04 17:12:08 +0200

    prepr.image: def get_image_md5_hash, get_image_path

--- src/preprocessing/image.py
+++ src/preprocessing/image.py
@@ -1,14 +1,20 @@
 import numpy as np
 import cv2
 import imagehash
+import hashlib
 from PIL import Image
 from pathlib import Path
 
 
-def load_image(row, color=True, folder = '../../Dataset/images/image_train'):
-    # OpenCV charge en BGR, donc l'ordre retourné est Bleu, Vert, Rouge.
+def get_image_path(row, folder = '../../Dataset/images/image_train'):
     filename = f"image_{row.imageid}_product_{row.productid}.jpg"
     path = Path(folder) / filename
+    return path
+
+
+def load_image(row, color=True, folder = '../../Dataset/images/image_train'):
+    '''OpenCV charge en BGR, donc l'ordre retourné est Bleu, Vert, Rouge.'''
+    path = get_image_path(row, folder)
     if color:
         color_arg = cv2.IMREAD_COLOR
     else:
@@ -46,14 +52,14 @@ def get_image_features_with_hash(row, folder='../../Dataset/images/image_train',
     # Créer un objet Image de Pillow et calculer le pHash.
     image_hash = str(imagehash.phash(Image.fromarray(img_rgb)))
 
-    # 3. Créer le masque de contenu (comme avant).
+    # 3. Créer le masque de contenu.
     img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
     gray_image_hash = str(imagehash.phash(Image.fromarray(img_gray)))
     content_mask = img_gray < threshold
 
     # Si l'image est vide de contenu, retourner les features possibles (le hash) et des zéros.
     if not np.any(content_mask):
-        # ... (le code pour retourner les zéros peut être factorisé, mais laissons-le pour la clarté)
+        # (le code pour retourner les zéros peut être factorisé, mais laissons-le pour la clarté)
         return {
             'image_hash': image_hash, 'gray_image_hash': gray_image_hash, 'mean_r': 0, 'mean_g': 0, 'mean_b': 0,
             'std_r': 0, 'std_g': 0, 'std_b': 0, 'median_r': 0, 'median_g': 0, 'median_b': 0,
@@ -86,3 +92,13 @@ def get_image_features_with_hash(row, folder='../../Dataset/images/image_train',
         'mean_gray': mean_gray, 'std_gray': std_gray, 'median_gray': median_gray,
         'essential_pixel_count': essential_pixel_count, 'x_min': x_min, 'y_min': y_min, 'x_max': x_max, 'y_max': y_max
     }
+
+
+def get_image_md5_hash(row, folder='../../Dataset/images/image_train') -> str:
+    # Calculer le hash MD5 de l'image.
+    image_path = get_image_path(row, folder)
+    with open(image_path, "rb") as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return file_hash.hexdigest()
