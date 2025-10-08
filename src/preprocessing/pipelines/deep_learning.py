e1871ed29cd3f9fe0520d838fff8b2085cf64794
val461 <6820182+val461@users.noreply.github.com>
2025-10-08 14:28:09 +0200

    DL.prepr: update default path

--- src/preprocessing/pipelines/deep_learning.py
+++ src/preprocessing/pipelines/deep_learning.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import joblib
 
 
-def load_preprocessors(names=['target','tabular','hash','text_vectorizer'],artifacts_folder='artifacts/on_images/deep_learning/v1'):
+def load_preprocessors(names=['target','tabular','hash','text_vectorizer'],artifacts_folder='artifacts/on_text_and_images/deep_learning/v1'):
     folder = Path(artifacts_folder) / "preprocessors"
     preprocessors = {}
     for name in names:
@@ -12,7 +12,7 @@ def load_preprocessors(names=['target','tabular','hash','text_vectorizer'],artif
     return preprocessors
 
 
-def save_preprocessors(preprocessors,artifacts_folder='artifacts/on_images/deep_learning/v1'):
+def save_preprocessors(preprocessors,artifacts_folder='artifacts/on_text_and_images/deep_learning/v1'):
     folder = Path(artifacts_folder) / "preprocessors"
     folder.mkdir(parents=True, exist_ok=True)
 
