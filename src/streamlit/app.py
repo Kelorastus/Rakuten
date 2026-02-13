73d46dad1ae2de415348e0325a7d2d94e6c1e512
val461 <6820182+val461@users.noreply.github.com>
2026-02-13 15:53:08 +0100

    streamlit app: reorder/rename/remove pages

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -31,12 +31,12 @@ st.set_page_config(
 
 # Import de toutes les fonctions show_*
 # Une fonction show_* par fichier de préférence, pour faciliter la collaboration
-from parts.problematique import show_problematique
-from parts.exploration import show_exploration
-from parts.preprocessing import show_preprocessing
-from parts.models import show_models
-from parts.analyse_meilleur_modele import show_analyse_meilleur_modele
-from parts.conclusion import show_conclusion
+# from parts.problematique import show_problematique
+# from parts.exploration import show_exploration
+# from parts.preprocessing import show_preprocessing
+# from parts.models import show_models
+# from parts.analyse_meilleur_modele import show_analyse_meilleur_modele
+# from parts.conclusion import show_conclusion
 from parts.ML_FR_EN import show_ML_FR_EN
 from parts.Deep_multimodal import show_Deep_multimodal
 
@@ -52,15 +52,15 @@ except ImportError as e:
 
 # Configuration de la navigation : partie → fonction montrant la partie
 PAGES = {
-    "🎯 Problématique": show_problematique,
-    "📊 Analyse exploratoire": show_exploration,
-    "⚙️ Preprocessing": show_preprocessing,
-    "🤖 Modèles & résultats": show_models,
-    "🚀 Démo interactive": show_demo,
-    "📈 Analyse du meilleur modèle": show_analyse_meilleur_modele,
-    "🎓 Conclusion & perspectives": show_conclusion,
-    "Machine_Learning_en_français_et_anglais": show_ML_FR_EN,
-    "Deep_multimodal": show_Deep_multimodal,
+    # "🎯 Problématique": show_problematique,
+    # "📊 Analyse exploratoire": show_exploration,
+    # "⚙️ Preprocessing": show_preprocessing,
+    # "🤖 Modèles & résultats": show_models,
+    "Machine Learning en français et anglais": show_ML_FR_EN,
+    "Deep multimodal DL4": show_Deep_multimodal,
+    "🚀 Démo interactive DL3": show_demo,
+    # "📈 Analyse du meilleur modèle": show_analyse_meilleur_modele,
+    # "🎓 Conclusion & perspectives": show_conclusion,
 }
 
 st.sidebar.title("Projet Rakuten")
